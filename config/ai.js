51649a50ac69c9b42b5c4808a0817a6b3bdf3eda
Rahul-Rasal <rasalrahul18@gmail.com>
2026-04-10 15:54:22 +0530

    10th commit

--- config/ai.js
+++ config/ai.js
@@ -1,5 +1,10 @@
-import { GoogleGenAI } from '@google/genai';
+// import { GoogleGenAI } from '@google/genai';
 
-const ai = new GoogleGenAI({ apiKey: process.env.GEMINI_API_KEY });
+// const ai = new GoogleGenAI({ apiKey: process.env.GEMINI_API_KEY });
 
-export default ai;
+// export default ai;
+import { GoogleGenerativeAI } from '@google/generative-ai';
+
+const genAI = new GoogleGenerativeAI(process.env.GEMINI_API_KEY);
+
+export default genAI;
